1f9793290d25ef72c06ddf1426ff66d6c8babab7
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-11-11 14:01:08 +0000

    Added my handlers

--- script.js
+++ script.js
@@ -25,3 +25,18 @@ function handleRestartGame() {
 }
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click, handleCellClick'));
 document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
+
+function handleCellClick(clickedCellEvent) {
+  const clickedCell = clickedCellEvent.target;
+
+  const clickedCellIndex = parseInt(
+    clickedCell.getAttribute('data-cell-index')
+  );
+
+  if (gameState[clickedCellIndex] !== "" || !gameActive){
+    return;
+  }
+
+  handleCellPlayed(clickedCell, clickedCellIndex);
+  handleResultValidation();
+}
