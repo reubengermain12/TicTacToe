6edd373d92d14fb71f6d1ed24f6329d35f7a3269
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-11-11 14:30:22 +0000

    Added a const variable

--- script.js
+++ script.js
@@ -45,3 +45,15 @@ function handleCellPlayed(clickedCell, clickedCellIndex) {
   gameState[clickedCellIndex] = currentPlayer;
   clickedCell.innerHTML = currentPlayer;
 }
+
+
+const winningConditions = [
+[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+[0, 3, 6],
+[1, 4, 7],
+[2, 5, 8],
+[0, 4, 8],
+[2, 4, 6]
+];
