c9b58835a86a252df6631fca676861699fbacd64
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-11-11 13:45:12 +0000

    Added my methods

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ let gameState = ["", "", "", "", "", "", "", "", "",];
 const winningMessage = () => `Player ${currentPlayer} has won!`;
 const drawMessage = () => `Game ended in a draw!`;
 const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+statusDisplay.innerHTML = currentPlayerTurn();
+function handleCellPlayed() {
+
+}
+function handlePlayerChange() {
+
+}
+function handleResultValidation() {
+
+}
+function handleCellClick() {
+
+}
+function handleRestartGame() {
+  
+}
